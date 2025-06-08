d01031d6a1ef29d1b479da090e6acdb3778a5c2c
benjamincudna2442 <customsmsbd72838@gmail.com>
2025-06-08 19:28:20 +0600

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+API_ID = int(getenv("API_ID", "26512884"))
+API_HASH = getenv("API_HASH", "c3f491cd59af263cfc249d3f93342ef8")
+BOT_TOKEN = getenv("BOT_TOKEN", "7941865929:AAEf7o5f-_VQKKWQKLs0qMOFHYwTi8Pjgwg")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "7303810912 5991909954").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://ytpremium4434360:zxx1VPDzGW96Nxm3@itssmarttoolbot.dhsl4.mongodb.net/?retryWrites=true&w=majority&appName=ItsSmartToolBot")
+LOG_GROUP = getenv("LOG_GROUP", "-1002888716176")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002740254173"))
